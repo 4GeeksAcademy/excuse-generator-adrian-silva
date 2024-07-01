c5f92fba781dd422680ac7dc03389dc7edd2bbdb
adrian-silva-uy <135650343+adrian-silva-uy@users.noreply.github.com>
2024-07-01 19:01:58 +0000

    excuse complete

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function excuseGenerator() {
+  let who = ["The dog ", "My grandma ", "The mailman ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "my phone ", "the car "];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let sentencias = [who, action, what, when];
+  let randomNum = "";
+  let phrase = "";
+
+  for (let i = 0; i < sentencias.length; i++) {
+    for (let j = 0; j < 1; j++) {
+      randomNum = Math.floor(Math.random() * sentencias[i].length);
+      phrase += sentencias[i][randomNum];
+    }
+  }
+  return phrase;
+}
+
+// function onLoad(){
+//     var excusa = document.getElementById('excuse');
+//     excusa.innerHTML = excuseGenerator();
+// }
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let excusa = excuseGenerator();
+  document.querySelector("#excuse").innerHTML = excusa;
 };
